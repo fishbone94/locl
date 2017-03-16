7fdf045e471c77aecfbc7e95a8fa840b054c9941
JonMercer <mercer.jon@gmail.com>
2017-03-16 15:17:29 -0700

    Moved user location and opens safari when link is sent

--- loclApp/loclApp/ViewController.swift
+++ loclApp/loclApp/ViewController.swift
@@ -8,20 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet weak var webView: UIWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let url = Bundle.main.url(forResource: "linkedCopyOfIndex", withExtension: "html")
         let request = NSURLRequest(url: url!)
+        webView.delegate = self
         webView.loadRequest(request as URLRequest)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // Swift 3
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        switch navigationType {
+        case .linkClicked:
+            // Open links in Safari
+            guard let url = request.url else { return true }
+            
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            } else {
+                // openURL(_:) is deprecated in iOS 10+.
+                UIApplication.shared.openURL(url)
+            }
+            return false
+        default:
+            // Handle other navigation types...
+            return true
+        }
     }
 
 
